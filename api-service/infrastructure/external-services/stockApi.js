0b45ffefe528d73dbfa6d1c6dd44c0a68c2b15c5
Victor Fernandes <victor.hmfd@gmail.com>
2024-05-16 20:22:47 -0300

    chore: set up env vars

--- api-service/infrastructure/external-services/stockApi.js
+++ api-service/infrastructure/external-services/stockApi.js
@@ -2,7 +2,7 @@ import axios from 'axios'
 
 class StockApiService {
   async fetchData(stockCode) {
-    const response = await axios.get(`http://stock-service:3002/stock?q=${stockCode}`);
+    const response = await axios.get(`${process.env.STOCK_API_ENDPOINT}?q=${stockCode}`);
     
     return response.data;
   }
--- stock-service/infrastructure/external-services/stockApi.js
+++ stock-service/infrastructure/external-services/stockApi.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 class StockApiService {
   async fetchData(stockCode) {
     if (!stockCode) return ''
-    const response = await axios.get(`https://stooq.com/q/l/?s=${stockCode}&f=sd2t2ohlcvn&h&e=csv`);
+    const response = await axios.get(`${process.env.STOCK_API_ENDPOINT}?s=${stockCode}&f=sd2t2ohlcvn&h&e=csv`);
     
     return response.data;
   }
